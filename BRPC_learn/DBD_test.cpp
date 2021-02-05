913a2523d1aa76ee0fa1b33ddd66fabc1bc107ac
yangziqi <yangziqi_i@didiglobal.com>
2021-02-06 01:22:53 +0800

    note-sync syncing

--- BRPC_learn/DBD_test.cpp
+++ BRPC_learn/DBD_test.cpp
@@ -75,7 +75,7 @@ int main() {
 
         });
     }
-    // 修改文件，读取文件
+    // 修改文件，读取文件12
     threads[9] = std::thread([&]{
         while(true){
             auto start = std::chrono::high_resolution_clock::now();
