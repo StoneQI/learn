656835691ee1abd7f6c8db6184b7d95833df4f3c
yangziqi <yangziqi_i@didiglobal.com>
2021-02-08 11:55:37 +0800

    note-sync syncing

--- BRPC_learn/MPSC.go
+++ BRPC_learn/MPSC.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-02-03 19:50:48
- * @LastEditTime: 2021-02-08 11:55:05
+ * @LastEditTime: 2021-02-08 11:55:24
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /learn/BRPC_learn/MPSC.go
@@ -38,9 +38,9 @@ func NewExecutionQueue(_func func(interface{})) *ExecutionQueue {
 
 type ExecutionQueue struct {
 	head          *TaskNode         `json:"head"`
-	_execute_func func(interface{}) `json:"_execute_func"`
-	locker        sync.Mutex        `json:"locker"`
-	pool          *sync.Pool        `json:"pool"`
+	_execute_func func(interface{}) `json:"-"`
+	locker        sync.Mutex        `json:"-"`
+	pool          *sync.Pool        `json:"-"`
 	// jsonVis       string            `json:-`
 }
 
