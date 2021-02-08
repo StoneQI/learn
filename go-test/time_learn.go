4486a4653e88a07737ebc41fd91164debdeef498
yangziqi <yangziqi_i@didiglobal.com>
2021-02-08 20:33:56 +0800

    note-sync syncing

--- go-test/time_learn.go
+++ go-test/time_learn.go
@@ -1,8 +1,21 @@
 /*
  * @Author: your name
  * @Date: 2021-02-08 20:32:50
- * @LastEditTime: 2021-02-08 20:32:50
+ * @LastEditTime: 2021-02-08 20:33:46
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /learn/go-test/time_learn.go
  */
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+
+	fmt.Printf(now)
+}
