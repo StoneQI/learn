1f616bfec75427aa69505c76a97e4c423cb6dcda
yangziqi <yangziqi_i@didiglobal.com>
2021-02-14 20:41:30 +0800

    note-sync syncing

--- go-test/time_learn.go
+++ go-test/time_learn.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-02-08 20:32:50
- * @LastEditTime: 2021-02-08 20:47:29
+ * @LastEditTime: 2021-02-14 20:41:20
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /learn/go-test/time_learn.go
@@ -11,11 +11,14 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
 	now := time.Now()
+	var a *sync.Mutex
+	a.lock()
 
 	fmt.Println(now)
 }
