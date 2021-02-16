66b652cc24f7a01203918d29ab668ee23aed9415
yangziqi <yangziqi_i@didiglobal.com>
2021-02-17 00:34:48 +0800

    note-sync syncing

---
+++ go-test/go_test.go
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-17 00:34:35
+ * @LastEditTime: 2021-02-17 00:34:37
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /learn/go-test/go_test.go
+ */
