3a947c94a4700825a9b3343a1284f9aa00d67bc3
yangziqi <yangziqi_i@didiglobal.com>
2021-02-17 00:43:10 +0800

    note-sync syncing

--- go-test/go_relect.go
+++ go-test/go_relect.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-02-17 00:34:35
- * @LastEditTime: 2021-02-17 00:41:47
+ * @LastEditTime: 2021-02-17 00:43:00
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /learn/go-test/go_test.go
@@ -67,7 +67,7 @@ func main() {
 
 	templateDaoMock1 := reflect.ValueOf(bbb).Elem()
 
-	cccc := templateDaoMock1.UnsafeAddr()
+	cccc := templateDaoMock1.Pointer()
 
 	_ = cccc
 	templateDaoMock2 := (*nonEmptyInterface)(unsafe.Pointer(templateDaoMock1.UnsafeAddr()))
