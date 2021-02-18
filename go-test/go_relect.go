29636f852eec59acf751d0f8c2e521b348456033
yangziqi <yangziqi_i@didiglobal.com>
2021-02-18 20:39:06 +0800

    note-sync syncing

--- go-test/go_relect.go
+++ go-test/go_relect.go
@@ -1,80 +1,37 @@
-/*
- * @Author: your name
- * @Date: 2021-02-17 00:34:35
- * @LastEditTime: 2021-02-17 00:43:00
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /learn/go-test/go_test.go
- */
 package main
 
 import (
-	"fmt"
-	"reflect"
 	"unsafe"
 )
 
-type nonEmptyInterface struct {
-	// see ../runtime/iface.go:/Itab
-	itab *struct {
-		ityp *rtype // static interface type
-		typ  *rtype // dynamic concrete type
-		hash uint32 // copy of typ.hash
-		_    [4]byte
-		fun  [100000]unsafe.Pointer // method table
+func a() int { return 1 }
+func b() int { return 2 }
+
+func rawMemoryAccess(b uintptr) []byte { // 获取b地址的内存块
+	return (*(*[0xFF]byte)(unsafe.Pointer(b)))[:] // 获取b对应的内存块，以便改写内容，从而达到替换目的
+}
+
+func assembleJump(f func() int) []byte { // 生成跳转到f函数所在位置的机器码
+	funcVal := *(*uintptr)(unsafe.Pointer(&f))
+	return []byte{
+		0x48, 0xC7, 0xC2,
+		byte(funcVal >> 0),
+		byte(funcVal >> 8),
+		byte(funcVal >> 16),
+		byte(funcVal >> 24), // MOV rdx, funcVal
+		0xFF, 0x22,          // JMP [rdx]
 	}
-	word unsafe.Pointer
-}
-type rtype struct {
-	size       uintptr
-	ptrdata    uintptr // number of bytes in the type that can contain pointers
-	hash       uint32  // hash of type; avoids computation in hash tables
-	tflag      uint8   // extra type information flags
-	align      uint8   // alignment of variable with this type
-	fieldAlign uint8   // alignment of struct field with this type
-	kind       uint8   // enumeration for C
-	// function for comparing objects of this type
-	// (ptr to object A, ptr to object B) -> ==?
-	equal     func(unsafe.Pointer, unsafe.Pointer) bool
-	gcdata    *byte // garbage collection data
-	str       int32 // string form
-	ptrToThis int32 // type for pointer to this type, may be zero
 }
 
-type a interface {
-	say()
-}
-type b struct {
-	aa int
-}
+func replace(orig, replacement func() int) {
+	bytes := assembleJump(replacement)
+	functionLocation := **(**uintptr)(unsafe.Pointer(&orig))
+	window := rawMemoryAccess(functionLocation)
 
-func (bb *b) say() {
-	fmt.Println(bb.aa)
-}
-
-type c struct {
-	aa string
-}
-
-func (bb *c) say() {
-	fmt.Println(bb.aa)
+	copy(window, bytes)
 }
 
 func main() {
-
-	bbb := &b{aa: 12}
-	ccc := &c{aa: "123"}
-
-	templateDaoMock1 := reflect.ValueOf(bbb).Elem()
-
-	cccc := templateDaoMock1.Pointer()
-
-	_ = cccc
-	templateDaoMock2 := (*nonEmptyInterface)(unsafe.Pointer(templateDaoMock1.UnsafeAddr()))
-
-	fpsTemplateDaoPtr := reflect.ValueOf(ccc).Elem()
-	fpsTemplateDaoUnPtr := (*nonEmptyInterface)(unsafe.Pointer(fpsTemplateDaoPtr.UnsafeAddr()))
-	fpsTemplateDaoUnPtr.itab.fun = templateDaoMock2.itab.fun
-
-	ccc.say()
+	replace(a, b)
+	print(a())
 }
